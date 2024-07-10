ef239b2f3e62155c8658c3ce2bad6cad156b23fc
kaito <muratakaito.3873@gmail.com>
2024-07-10 04:24:00 +0000

    first commit

---
+++ diary-app/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>このアプリは日々の取り組みを記録するアプリであり、<br>記録した内容を分析して自己PRを生成するアプリです。</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
